f06f2fc1531d1c83c4b4d823b54944aa1ef4df08
Sofia Tenório <sofiaestudante29@gmail.com>
2024-10-21 10:00:46 -0300

    adicionando processador e leitor de dados

--- internal/handlers/file_handler.go
+++ internal/handlers/file_handler.go
@@ -1,57 +1,42 @@
 package handlers
 
 import (
+    "bufio"
     "context"
     "encoding/csv"
-    "io"
+    "fmt"
     "net/http"
-    "go_project/internal/database"
-    "go_project/pkg/models"
-    "go_project/pkg/utils"
     "log"
     "strconv"
     "strings"
     "time"
+    "seu_projeto/internal/database"
+    "seu_projeto/pkg/utils"
 )
 
 func CarregarArquivo(db *database.Database) http.HandlerFunc {
     return func(w http.ResponseWriter, r *http.Request) {
         if r.Method != http.MethodPost {
+            log.Println("Método não permitido")
             http.Error(w, "Método não permitido", http.StatusMethodNotAllowed)
             return
         }
 
-        // Limita o tamanho do corpo da requisição para evitar uploads muito grandes
-        r.Body = http.MaxBytesReader(w, r.Body, 10<<20) // 10 MB
-
-        err := r.ParseMultipartForm(10 << 20) // 10 MB
+        err := r.ParseMultipartForm(0)
         if err != nil {
-            http.Error(w, "Tamanho do arquivo excede o limite permitido", http.StatusBadRequest)
+            log.Printf("Falha ao processar o arquivo: %v", err)
+            http.Error(w, "Falha ao processar o arquivo", http.StatusBadRequest)
             return
         }
 
-        file, header, err := r.FormFile("file")
+        file, _, err := r.FormFile("file")
         if err != nil {
+            log.Printf("Falha ao carregar o arquivo: %v", err)
             http.Error(w, "Falha ao carregar o arquivo", http.StatusBadRequest)
             return
         }
         defer file.Close()
 
-        // Detecta o tipo de arquivo (CSV ou TXT)
-        fileType := header.Header.Get("Content-Type")
-        var leitor *csv.Reader
-
-        if strings.Contains(fileType, "text/plain") || strings.HasSuffix(header.Filename, ".txt") {
-            leitor = csv.NewReader(file)
-            leitor.Comma = '\t' // Assume que o TXT está separado por tabulação
-        } else {
-            leitor = csv.NewReader(file)
-            leitor.Comma = ',' // Assume que o CSV está separado por vírgula
-        }
-
-        leitor.TrimLeadingSpace = true
-
-        // Inicia uma transação
         tx, err := db.Pool.Begin(context.Background())
         if err != nil {
             log.Printf("Erro ao iniciar a transação: %v", err)
@@ -69,41 +54,58 @@ func CarregarArquivo(db *database.Database) http.HandlerFunc {
             return
         }
 
-        // Lê o cabeçalho e ignora
-        _, err = leitor.Read()
-        if err != nil {
-            log.Printf("Erro ao ler o cabeçalho do arquivo: %v", err)
-            http.Error(w, "Erro ao processar o arquivo", http.StatusInternalServerError)
-            return
-        }
+        scanner := bufio.NewScanner(file)
+        firstLine := true
+        var totalProcessed, totalSkipped int
 
-        for {
-            registro, err := leitor.Read()
-            if err == io.EOF {
-                break
-            }
-            if err != nil {
-                log.Printf("Erro ao ler o arquivo: %v", err)
-                http.Error(w, "Erro ao processar o arquivo", http.StatusInternalServerError)
-                return
-            }
+        for scanner.Scan() {
+            linha := strings.TrimSpace(scanner.Text())
 
-            // Verifica se o registro tem pelo menos 8 campos
-            if len(registro) < 8 {
-                log.Printf("Registro incompleto: %v", registro)
+            // Ignorar a primeira linha se for cabeçalho
+            if firstLine {
+                firstLine = false
                 continue
             }
 
-            // Processa cada registro
+            // Determinar se é CSV ou TXT e ajustar delimitadores
+            var reader *csv.Reader
+            if strings.Contains(linha, ",") {
+                reader = csv.NewReader(strings.NewReader(linha))
+                reader.Comma = ','
+            } else {
+                reader = csv.NewReader(strings.NewReader(linha))
+                reader.Comma = ' ' // espaço como delimitador para o TXT
+                reader.FieldsPerRecord = -1 // permite que tenha um número variável de campos
+            }
+
+            // Usar o reader para ler os registros
+            registro, err := reader.Read()
+            if err == csv.ErrFieldCount {
+                log.Printf("Número de campos inválido em: %v", linha)
+                totalSkipped++
+                continue
+            } else if err != nil {
+                log.Printf("Erro ao ler registro: %v", err)
+                break // Saia do loop ao encontrar EOF ou erro
+            }
+
+            if len(registro) < 8 {
+                log.Printf("Registro incompleto: %v", registro)
+                totalSkipped++
+                continue
+            }
+
             cpf := utils.RemoverCaracteresEspeciais(registro[0])
             private, err := strconv.Atoi(registro[1])
             if err != nil {
-                log.Printf("Erro ao converter PRIVATE: %v", err)
+                log.Printf("Erro ao converter PRIVATE: %v, registro: %v", err, registro)
+                totalSkipped++
                 continue
             }
             incompleto, err := strconv.Atoi(registro[2])
             if err != nil {
-                log.Printf("Erro ao converter INCOMPLETO: %v", err)
+                log.Printf("Erro ao converter INCOMPLETO: %v, registro: %v", err, registro)
+                totalSkipped++
                 continue
             }
 
@@ -111,7 +113,7 @@ func CarregarArquivo(db *database.Database) http.HandlerFunc {
             if strings.ToUpper(registro[3]) != "NULL" && strings.TrimSpace(registro[3]) != "" {
                 parsedDate, err := time.Parse("2006-01-02", registro[3])
                 if err != nil {
-                    log.Printf("Erro ao parsear DATA DA ÚLTIMA COMPRA: %v", err)
+                    log.Printf("Erro ao parsear DATA DA ÚLTIMA COMPRA: %v, registro: %v", err, registro)
                 } else {
                     dateStr := parsedDate.Format("2006-01-02")
                     dataDaUltimaCompra = &dateStr
@@ -120,9 +122,10 @@ func CarregarArquivo(db *database.Database) http.HandlerFunc {
 
             var ticketMedio *float64
             if strings.ToUpper(registro[4]) != "NULL" && strings.TrimSpace(registro[4]) != "" {
-                tm, err := strconv.ParseFloat(registro[4], 64)
+                ticketStr := strings.ReplaceAll(registro[4], ",", ".")
+                tm, err := strconv.ParseFloat(ticketStr, 64)
                 if err != nil {
-                    log.Printf("Erro ao parsear TICKET MÉDIO: %v", err)
+                    log.Printf("Erro ao parsear TICKET MÉDIO: %v, registro: %v", err, registro)
                 } else {
                     ticketMedio = &tm
                 }
@@ -130,9 +133,10 @@ func CarregarArquivo(db *database.Database) http.HandlerFunc {
 
             var ticketDaUltimaCompra *float64
             if strings.ToUpper(registro[5]) != "NULL" && strings.TrimSpace(registro[5]) != "" {
-                tdc, err := strconv.ParseFloat(registro[5], 64)
+                ticketStr := strings.ReplaceAll(registro[5], ",", ".")
+                tdc, err := strconv.ParseFloat(ticketStr, 64)
                 if err != nil {
-                    log.Printf("Erro ao parsear TICKET DA ÚLTIMA COMPRA: %v", err)
+                    log.Printf("Erro ao parsear TICKET DA ÚLTIMA COMPRA: %v, registro: %v", err, registro)
                 } else {
                     ticketDaUltimaCompra = &tdc
                 }
@@ -151,18 +155,27 @@ func CarregarArquivo(db *database.Database) http.HandlerFunc {
             }
 
             if !ValidarCPF(cpf) {
-                log.Printf("CPF inválido: %v", cpf)
+                log.Printf("CPF inválido: %v, registro: %v", cpf, registro)
+                totalSkipped++
                 continue
             }
 
             _, err = tx.Exec(context.Background(), stmt.SQL, cpf, private, incompleto, dataDaUltimaCompra, ticketMedio, ticketDaUltimaCompra, lojaMaisFrequente, lojaDaUltimaCompra)
             if err != nil {
-                log.Printf("Falha ao inserir registro: %v", err)
+                log.Printf("Falha ao inserir registro: %v, erro: %v", registro, err)
+                totalSkipped++
                 continue
             }
+
+            totalProcessed++
         }
 
-        // Confirma a transação
+        if err := scanner.Err(); err != nil {
+            log.Printf("Erro ao ler o arquivo: %v", err)
+            http.Error(w, "Erro ao processar o arquivo", http.StatusInternalServerError)
+            return
+        }
+
         err = tx.Commit(context.Background())
         if err != nil {
             log.Printf("Erro ao confirmar a transação: %v", err)
@@ -170,7 +183,8 @@ func CarregarArquivo(db *database.Database) http.HandlerFunc {
             return
         }
 
+        log.Printf("Arquivo processado com sucesso: %d registros processados, %d registros ignorados.", totalProcessed, totalSkipped)
         w.WriteHeader(http.StatusOK)
-        w.Write([]byte("Arquivo processado com sucesso"))
+        w.Write([]byte(fmt.Sprintf("Arquivo processado com sucesso: %d registros processados, %d registros ignorados.", totalProcessed, totalSkipped)))
     }
 }
