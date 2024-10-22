80b6e17335f73f9fc662d6da352ad6885df27fc8
Sofia Tenório <sofiaestudante29@gmail.com>
2024-10-22 09:20:08 -0300

    funcoes utilitarias para app

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -1,17 +1,50 @@
 package utils
 
 import (
+    "log"
+    "strconv"
     "strings"
-    "golang.org/x/text/unicode/norm"
     "unicode"
+    "time"
 )
 
-// NormalizarString converte a string para maiúsculas e normaliza os caracteres Unicode
-func NormalizarString(s string) string {
-    return strings.ToUpper(norm.NFC.String(s))
+// ValidarData verifica se uma string é uma data válida e retorna um ponteiro para a string formatada, ou nil se for inválida
+func ValidarData(dataStr string) *string {
+    if dataStr == "NULL" || dataStr == "" {
+        return nil
+    }
+    
+    // Tentar converter a string em um formato de data padrão (ajuste o formato de acordo com seu caso)
+    const formato = "2006-01-02" // Formato de data ISO (ex: 2013-06-12)
+    _, err := time.Parse(formato, dataStr)
+    if err != nil {
+        log.Printf("Data inválida: %v", dataStr)
+        return nil
+    }
+    
+    return &dataStr
 }
 
-// RemoverCaracteresEspeciais remove caracteres não numéricos de uma string
+// ParsearValorMonetario converte uma string de valor monetário em float64, ou retorna nil se não for válido
+func ParsearValorMonetario(valorStr string) *float64 {
+    if valorStr == "NULL" || valorStr == "" {
+        return nil
+    }
+
+    // Substitui vírgula por ponto, caso necessário
+    valorStr = strings.ReplaceAll(valorStr, ",", ".")
+
+    // Tentar converter para float64
+    valor, err := strconv.ParseFloat(valorStr, 64)
+    if err != nil {
+        log.Printf("Erro ao converter valor monetário: %v", valorStr)
+        return nil
+    }
+
+    return &valor
+}
+
+
 func RemoverCaracteresEspeciais(s string) string {
     var builder strings.Builder
     for _, r := range s {
